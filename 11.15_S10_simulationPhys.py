c324463f28e5d85c1a698059a1551c5a6e1f04a1
Rinat1234567890 <75635014+Rinat1234567890@users.noreply.github.com>
2020-12-12 17:13:00 +0100

    Update 11.15_S10_simulationPhys.py
    Ajout de commentaires supplémentaires

--- 11.15_S10_simulationPhys.py
+++ 11.15_S10_simulationPhys.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 """
-***Variables de base: la barge est supposée stable, carrée et n'oscille pas.***
+***Variables de base: la barge est supposÃ©e stable, carrÃ©e et n'oscille pas.***
 """
 
 long   = 0.60                  #Longueur de la barge. (m)
@@ -20,7 +20,7 @@ coeff_amort=0.6                #Coefficient d'amortissement
 mass_tot=6.3                   #Masse totale de la grue et plateforme (kg) 
 mass_obj=0.130                 #Masse de l'objet deplace (kg)
 
-### Centre de gravite. Il faut les calculer à la main.
+### Centre de gravite. Il faut les calculer Ã  la main.
 
 cg_x_0 = 0.10
 cg_y_0 = 0.15
@@ -53,7 +53,7 @@ soulev2= np.empty_like(t)
 submer2= np.empty_like(t)
 haut_enf=mass_tot/(rho_eau*long**2)
 
-### Fonction de rotation. Elle met �  jour les valeurs nécessaires pour le calcul.
+### Fonction de rotation. Elle met Ã  jour les valeurs nÃ©cessaires pour le calcul.
 
 def rotation(i):
     global delta_h
@@ -106,42 +106,49 @@ def simulation():
         alpha[i]=mf_tot/mass_tot #Ceci calcule le couple de redressement
         omega[i+1]=omega[i]+alpha[i]*dt
         theta[i+1]=theta[i]+omega[i]*dt
-        alpha[i+1]=alpha[i]
-        if abs(dist[i]-dist_t)>=abs(2*dist_v*dt):
+        alpha[i+1]=alpha[i] 
+        #Ces trois conditions correspondent au calcul par iteration de l'angle, pulsation et acceleration angulaires dans le modele physique.
+        if abs(dist[i]-dist_t)>=abs(2*dist_v*dt): #Cette condition 'if' calcule le deplacement de la charge en fonction du temps.
             dist[i+1]=dist[i]+dist_v*dt
         else:
-            dist[i+1]=dist_t
+            dist[i+1]=dist_t 
     haut_max=m.atan(2*(haut-haut_enf)/long)
     delta_h_max=m.atan(2*(haut_enf)/long)
+    #Calcul des angles correspondant aux situations critiques du bateau (soulevement et submersion). Leurs noms sont un peu trompeurs.
     
     for i in range(len(t)):
         soulev[i]=(delta_h_max)
         submer[i]=(haut_max)
         soulev2[i]=-(delta_h_max)
         submer2[i]=-(haut_max)
+        #Dessin des points critiques
         
 def graphiques(): 
     """
     Cette fonction dessine et legende les graphiques.
     """
-    plt.figure(1)
-    plt.subplot(3,1,1)
+    plt.figure(1) #Creation d'une image blanche sur laquelle se trouveront les graphiques
+    plt.subplot(3,1,1) #Creation du graphique indiquant l'evolution de l'angle au cours du temps
     plt.plot(t,theta, label="angle")
     plt.plot(t,soulev,color='orange',linestyle='dashed')
-    plt.plot(t,submer,color='red',linestyle='dashed')
-
+    plt.plot(t,submer,color='red',linestyle='dashed') 
+    #Les trois .plot() dessinent les angles du bateau et les angles maximales dans le premier graphique
+    
     print("Orange == Soulevement\nRouge == Submersion")
-    plt.legend()
-    plt.subplot(3,1,2)
+    plt.legend() #Commande qui legende le graphique.
+    plt.subplot(3,1,2) #Creation du deuxieme graphique: celui ci indique l'evolution de la pulsation angulaire au cours du temps
     plt.plot(t,omega, label="v. angulaire")
     plt.legend()
-    plt.subplot(3,1,3)
+    plt.subplot(3,1,3) #creation du troisieme graphique: celui ci indique l'evolution de l'acceleration angulaire au cours du temps
     plt.plot(t,alpha, label="a. angulaire")
     plt.legend()
-    plt.show()
+    plt.show() #Renvoie l'image avec les graphiques
 
 simulation()
 graphiques()
+#Etant donne que ce fichier n'est pas cense etre importe, la condition 'if __name__ == "__main__"' n'a pas ete introduite.
 
 print("Angle final:",theta[29999]*180/m.pi,"degres")
-print("Submersion:",submer[29999]*180/m.pi,"degres")
+print("Soulevement:",soulev[29999]*180/m.pi,"degres")
+print("Submersion:",submer[29999]*180/m.pi,"degres") 
+#Les trois print() renvoient les dernieres valeurs pour l'angle theta et les angles maximales.
